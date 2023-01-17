f839c8c51cdbc01487288d8362b919fc6f85157b
LucasGdosR <109366863+LucasGdosR@users.noreply.github.com>
2023-01-17 07:10:12 -0300

    implementa alerta de "esqueceu sua senha"

--- lab-medical/src/app/pages/login/login.component.ts
+++ lab-medical/src/app/pages/login/login.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-
+  notImplemented(){
+    alert('A funcionalidade de "esqueceu sua senha" ainda não foi implementada. Crie um novo usuário!')
+  }
 }
