630abc6531350f8dbfb1bf630850217c1a653f09
LucasGdosR <109366863+LucasGdosR@users.noreply.github.com>
2023-01-18 20:31:20 -0300

    cria cards da pagina inicial

--- lab-medical/src/app/pages/inicio/inicio.component.ts
+++ lab-medical/src/app/pages/inicio/inicio.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DatabaseService } from 'src/app/services/database.service';
 
 @Component({
   selector: 'app-inicio',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./inicio.component.scss']
 })
 export class InicioComponent {
+  constructor(private database: DatabaseService) {}
 
+
+  statistics = [
+    { 
+      "title" : "Pacientes",
+      "quantity" : this.database.patients.length,
+      "icon" : "../../../../assets/pacientes.png"
+    },
+    {
+      "title" : "Consultas",
+      "quantity" : this.database.appointments.length,
+      "icon" : "../../../../assets/consulta-medica.png"
+    },
+    {
+      "title" : "Exames",
+      "quantity" : this.database.exams.length,
+      "icon" : "../../../../assets/prontuario-medico.png"
+    }
+  ]
 }
