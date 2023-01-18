3994f5024a045f0b55419e7a0bd78e1f6ce8c411
LucasGdosR <109366863+LucasGdosR@users.noreply.github.com>
2023-01-18 09:07:17 -0300

    implementa sidebar sem roteamento

--- lab-medical/src/app/components/menu/menu.component.ts
+++ lab-medical/src/app/components/menu/menu.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss']
 })
-export class MenuComponent {
+export class MenuComponent implements AfterViewInit{
+  sidebarState : string = "Expandir"
 
+  shrinkOrExpand(){
+    if (this.sidebarState === "Encolher")
+      this.sidebarState = "Expandir"
+
+    else this.sidebarState = "Encolher"
+  }
+
+  ngAfterViewInit(): void {
+    document.getElementById("collapseButton")?.click()
+  }
+  
 }
