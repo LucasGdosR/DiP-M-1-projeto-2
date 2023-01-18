a0e1a695f60a88638c96fddc56a0c532f1df8307
LucasGdosR <109366863+LucasGdosR@users.noreply.github.com>
2023-01-18 07:32:44 -0300

    cria o componente menu

--- lab-medical/src/app/app.module.ts
+++ lab-medical/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
