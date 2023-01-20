7665e0db6391ed9256dd8958e4fc5df529bf2b63
LucasGdosR <109366863+LucasGdosR@users.noreply.github.com>
2023-01-20 11:29:33 -0300

    implementa pipe de nascimento para idade

--- lab-medical/src/app/pipes/age.pipe.ts
+++ lab-medical/src/app/pipes/age.pipe.ts
@@ -5,8 +5,15 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class AgePipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(dataDeNascimento: Date): string {
+    const today = new Date()
+    let age = today.getFullYear() - dataDeNascimento.getFullYear()
+    const monthDifference = today.getMonth() - dataDeNascimento.getMonth()
+
+    if (monthDifference < 0 || monthDifference === 0 && today.getDate() < dataDeNascimento.getDate())
+      age--
+      
+    return age + " Anos";
   }
 
 }
