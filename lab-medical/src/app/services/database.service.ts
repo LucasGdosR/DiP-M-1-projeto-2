5ae35d78cf7b34f9f359d3f3afa20e7f99cc9b3c
LucasGdosR <109366863+LucasGdosR@users.noreply.github.com>
2023-01-20 17:23:57 -0300

    implementa serviço de localStorage

--- lab-medical/src/app/services/database.service.ts
+++ lab-medical/src/app/services/database.service.ts
@@ -8,9 +8,36 @@ import Patient from '../interfaces/patient.interface';
 })
 export class DatabaseService {
 
-  patients: Patient[] = []
-  appointments: Appointment[] = []
-  exams: Exam[] = []
+  patients!: Patient[]
+  appointments!: Appointment[]
+  exams!: Exam[]
 
-  constructor() { }
+  nextPatientID!: number
+  nextAppointmentID!: number
+  nextExamID!: number
+
+  constructor() {
+    const patients = localStorage.getItem('patients')
+    this.patients = patients ? JSON.parse(patients) : []
+
+    const appointments = localStorage.getItem('appointments')
+    this.appointments = appointments ? JSON.parse(appointments) : []
+
+    const exams = localStorage.getItem('exams')
+    this.exams = exams ? JSON.parse(exams) : []
+
+    const nextPatientID = localStorage.getItem('nextPatientID')
+    this.nextPatientID = nextPatientID ? JSON.parse(nextPatientID) : 0
+
+    const nextAppointmentID = localStorage.getItem('nextAppointmentID')
+    this.nextAppointmentID = nextAppointmentID ? JSON.parse(nextAppointmentID) : 0
+
+    const nextExamID = localStorage.getItem('nextExamID')
+    this.nextExamID = nextExamID ? JSON.parse(nextExamID) : 0
+   }
+
+   persist(localKey: string, value: any) {
+    localStorage.setItem(localKey, JSON.stringify(value))
+   }
+
 }
