38ba5973fffde32e4b27e063942bb154da0e9077
LucasGdosR <109366863+LucasGdosR@users.noreply.github.com>
2023-01-21 08:41:00 -0300

    implementa lógica de busca de paciente

--- lab-medical/src/app/pages/cadastrar-consulta/cadastrar-consulta.component.ts
+++ lab-medical/src/app/pages/cadastrar-consulta/cadastrar-consulta.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DatabaseService } from 'src/app/services/database.service';
 
 @Component({
   selector: 'app-cadastrar-consulta',
@@ -6,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./cadastrar-consulta.component.scss']
 })
 export class CadastrarConsultaComponent {
+  constructor(private database: DatabaseService) {}
 
+  foundPatient = this.database.patients
+  pacienteBuscado: string = ""
+  formEnabled: boolean = false
+  patientId!: number
+  patientFullName: string = ""
+
+  findPatient() {
+   this.foundPatient = this.database.patients.filter(patient => patient.nomeCompleto.toLocaleLowerCase().includes(this.pacienteBuscado.toLocaleLowerCase()))
+
+   if (this.foundPatient.length === 1)
+     this.enableForm()
+
+   if (this.foundPatient.length === 0)
+     return alert("Nenhum paciente com esse nome.")
+
+   if (this.foundPatient.length > 1) {
+     let names = ""
+     this.foundPatient.forEach(patient => names += patient.nomeCompleto + "\n")
+     alert("Qual " + this.pacienteBuscado + " ? Encontramos:\n" + names)
+   }
+  }
+
+  enableForm() {
+    this.formEnabled = true
+    this.patientId = this.foundPatient[0].id
+    this.patientFullName = this.foundPatient[0].nomeCompleto
+  }
 }
