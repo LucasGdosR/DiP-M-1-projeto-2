c5f20733458bb2f9652e39890b49000ad3fb507e
LucasGdosR <109366863+LucasGdosR@users.noreply.github.com>
2023-01-21 13:32:19 -0300

    cria formulario com validacao e persistencia

--- lab-medical/src/app/pages/cadastrar-exame/cadastrar-exame.component.ts
+++ lab-medical/src/app/pages/cadastrar-exame/cadastrar-exame.component.ts
@@ -1,4 +1,9 @@
+import { Time } from '@angular/common';
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import Exam from 'src/app/interfaces/exam.interface';
+import Patient from 'src/app/interfaces/patient.interface';
+import { DatabaseService } from 'src/app/services/database.service';
 
 @Component({
   selector: 'app-cadastrar-exame',
@@ -6,5 +11,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./cadastrar-exame.component.scss']
 })
 export class CadastrarExameComponent {
+  constructor(private database: DatabaseService) {}
 
+  formEnabled: boolean = false
+  patientId!: number
+  patientFullName: string = ""
+
+  examName: string = ""
+  examDate: Date = new Date()
+  examTime: Time = {hours: 8, minutes: 0}
+  examType: string = ""
+  laboratory: string = ""
+  url?: string
+  results: string = ""
+
+  enableForm(foundPatient: Patient) {
+    this.formEnabled = true
+    this.patientId = foundPatient.id
+    this.patientFullName = foundPatient.nomeCompleto
+  }
+
+  registerExam(form: NgForm) {
+    const exam: Exam = {
+      idDoPaciente: this.patientId,
+      idDoExame: this.database.nextExamID++,
+      nome: this.examName,
+      dataDoExame: this.examDate,
+      horario: this.examTime,
+      tipoDoExame: this.examType,
+      laboratorio: this.laboratory,
+      url: this?.url,
+      resultados: this.results
+    }
+
+    this.database.exams.push(exam)
+    form.reset()
+    this.database.persist('exams', this.database.exams)
+    this.database.persist('nextExamID', this.database.nextExamID)
+
+    alert("Exame cadastrado com sucesso.")
+  }
 }
