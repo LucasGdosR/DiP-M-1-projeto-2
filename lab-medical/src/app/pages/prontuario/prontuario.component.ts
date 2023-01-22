2d0c6a494609a2495d2569668e08b3c52345e62b
LucasGdosR <109366863+LucasGdosR@users.noreply.github.com>
2023-01-22 16:26:09 -0300

    implementa a lógica de busca de paciente

--- lab-medical/src/app/pages/prontuario/prontuario.component.ts
+++ lab-medical/src/app/pages/prontuario/prontuario.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import Appointment from 'src/app/interfaces/appointment.interface';
+import Exam from 'src/app/interfaces/exam.interface';
+import Patient from 'src/app/interfaces/patient.interface';
+import { DatabaseService } from 'src/app/services/database.service';
 
 @Component({
   selector: 'app-prontuario',
@@ -6,5 +10,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./prontuario.component.scss']
 })
 export class ProntuarioComponent {
-  placeholder: string = "placeholder"
+  patientId!: number
+  patient: Patient | undefined = this.database.patients.find(patient => patient.id === this.patientId)
+  patientName: string = this.patient!.nomeCompleto
+  insurance: string = this.patient?.convenio ? this.patient.convenio : "Não Possui"
+  emergencyContact: string = this.patient!.contatoDeEmergencia
+  allergyList: string | undefined = this.patient?.listaDeAlergias
+  careList: string | undefined = this.patient?.listaDeCuidadosEspecificos
+  
+  appointments: Appointment[] = this.database.appointments.filter(appointment => appointment.idDoPaciente === this.patientId)
+  exams: Exam[] = this.database.exams.filter(exam => exam.idDoPaciente === this.patientId)
+  
+  constructor(private database: DatabaseService) {}
+  
 }
