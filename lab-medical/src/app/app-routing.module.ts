9dc4e0419496174ecfcb30b401fdd93fd5627990
LucasGdosR <109366863+LucasGdosR@users.noreply.github.com>
2023-01-22 17:11:22 -0300

    implementa roteamento entre paginas

--- lab-medical/src/app/app-routing.module.ts
+++ lab-medical/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastrarConsultaComponent } from './pages/cadastrar-consulta/cadastrar-consulta.component';
+import { CadastrarExameComponent } from './pages/cadastrar-exame/cadastrar-exame.component';
+import { CadastrarPacienteComponent } from './pages/cadastrar-paciente/cadastrar-paciente.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { ListarProntuariosComponent } from './pages/listar-prontuarios/listar-prontuarios.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ProntuarioComponent } from './pages/prontuario/prontuario.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'inicio', component: InicioComponent },
+  { path: 'cadastrar-paciente', component: CadastrarPacienteComponent },
+  { path: 'cadastrar-consulta', component: CadastrarConsultaComponent },
+  { path: 'cadastrar-exame', component: CadastrarExameComponent },
+  { path: 'listar-prontuarios', component: ListarProntuariosComponent},
+  { path: 'listar-prontuarios/:id', component: ProntuarioComponent },
+  { path: '**', redirectTo: '/inicio', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
