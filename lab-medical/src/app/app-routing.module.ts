c0e827b05ed1fed3ff30499d2c113e1a6db3b3bd
LucasGdosR <109366863+LucasGdosR@users.noreply.github.com>
2023-01-23 11:34:09 -0300

    implementa guardas de rota

--- lab-medical/src/app/app-routing.module.ts
+++ lab-medical/src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
+import { LoggedOutGuard } from './guards/logged-out.guard';
 import { CadastrarConsultaComponent } from './pages/cadastrar-consulta/cadastrar-consulta.component';
 import { CadastrarExameComponent } from './pages/cadastrar-exame/cadastrar-exame.component';
 import { CadastrarPacienteComponent } from './pages/cadastrar-paciente/cadastrar-paciente.component';
@@ -9,13 +11,13 @@ import { LoginComponent } from './pages/login/login.component';
 import { ProntuarioComponent } from './pages/prontuario/prontuario.component';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'inicio', component: InicioComponent },
-  { path: 'cadastrar-paciente', component: CadastrarPacienteComponent },
-  { path: 'cadastrar-consulta', component: CadastrarConsultaComponent },
-  { path: 'cadastrar-exame', component: CadastrarExameComponent},
-  { path: 'listar-prontuarios', component: ListarProntuariosComponent},
-  { path: 'listar-prontuarios/:id', component: ProntuarioComponent },
+  { path: 'login', component: LoginComponent, canActivate: [LoggedOutGuard] },
+  { path: 'inicio', component: InicioComponent, canActivate: [AuthGuard] },
+  { path: 'cadastrar-paciente', component: CadastrarPacienteComponent, canActivate: [AuthGuard] },
+  { path: 'cadastrar-consulta', component: CadastrarConsultaComponent, canActivate: [AuthGuard] },
+  { path: 'cadastrar-exame', component: CadastrarExameComponent, canActivate: [AuthGuard] },
+  { path: 'listar-prontuarios', component: ListarProntuariosComponent, canActivate: [AuthGuard] },
+  { path: 'listar-prontuarios/:id', component: ProntuarioComponent, canActivate: [AuthGuard] },
   { path: '**', redirectTo: '/inicio', pathMatch: 'full' }
 ];
 
